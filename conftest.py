2f9ef3b89a559b57a428cff821e05743d81aee59
Sergei <PopkovSergei0805@yandex.ru>
2020-03-23 14:21:11 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope="session")
 def browser():
     # browser = webdriver.Chrome(ChromeDriverManager().install())
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome("./chromedriver.exe")
     browser.maximize_window()
     yield browser
     print("\nquit browser..")
